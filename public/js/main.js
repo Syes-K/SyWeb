2742975a73d22f167bbf8b3e36456533e88aff47
yongsheng.kuang <yongsheng.kuang@ptthink.com>
2016-01-12 09:23:34 +0800

    初建 服务端路由系统和前段api路由

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by ptmind on 2016/1/11.
+ */
+define(function (require, exports, module) {
+    var _input = document.getElementById('txt1');
+    var content1 = document.getElementById('content1');
+    var r = require('route');
+    var user = r.createRoute('/user');
+    user.on('/test', function (result) {
+        showMsg('[ ] message:', result);
+    });
+    document.getElementById('form1').addEventListener('submit', function (e) {
+        user.send('/test', _input.value);
+        showMsg('[ ] sending', {content: _input.value, method: '/test'});
+        _input.value = '';
+        e.preventDefault();
+    })
+    var showMsg = function (m, p) {
+        p = (p === undefined) ? '' : JSON.stringify(p);
+        content1.innerHTML += ( '<code>' + m + ' ' + p + '</code>');
+        content1.innerHTML += ("<br/>");
+        content1.scrollTop += 10000;
+    };
+});
