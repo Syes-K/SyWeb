d5cff772d3e20ac543513f4ecdb606b0f83bdead
yongsheng.kuang <yongsheng.kuang@ptthink.com>
2016-01-21 14:48:16 +0800

    index页的路径修改

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 var httpServer=  require('http').createServer();
 var nodeStatic=  require('node-static');
-var fileServer = new nodeStatic.Server('./public',{cache:3600});//cache 3600 hours
+var fileServer = new nodeStatic.Server('./public_pro',{cache:3600});//cache 3600 hours
 var user = require('./socket/routes/user');
 //静态资源服务器
 httpServer.addListener('request', function (req,res) {
